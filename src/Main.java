de4e07f821aec08da40493ef0f3005de2738451e
Stephen Fox <stephenfox995@hotmail.com>
2016-11-16 13:18:13 +0000

    Update error message for incorrect bid prices

--- src/Main.java
+++ src/Main.java
@@ -7,29 +7,43 @@ import java.net.UnknownHostException;
  */
 public class Main {
   private static InetAddress host;
-  private static int PORT_NUMBER = 8080;
-  private static boolean runAsServer = false;
+
 
   private static String commandLineArgOptions = "Options:\n" +
-          "\tserver: Run program as server.\n" +
-          "\tclient: Run program as client.";
+          "\tserver <port>: Run program as server.\n" +
+          "\tclient <port>: Run program as client.";
 
   public static void main(String[] args) {
-    if (args.length <= 0) {
-      System.out.println("No command line arguments specified.");
+    int portNumber = 0;
+    if (args.length != 2) {
+      System.out.println("Invalid command line arguments specified.");
       System.out.println(commandLineArgOptions);
+      System.exit(1);
     }
-    else if (args[0].equals("server") ) {
+    else {
+      try {
+        portNumber = Integer.parseInt(args[1]);
+        if (portNumber < 0 && portNumber > 65536) {
+          System.out.println("Invalid port number");
+          System.exit(1);
+        }
+      } catch (NumberFormatException e) {
+        System.out.println("<port> argument must be a number.");
+        System.exit(1);
+      }
+    }
+
+    if (args[0].equals("server")) {
       Server server = Server.sharedServer();
-      server.beginListening(PORT_NUMBER);
+      server.beginListening(portNumber);
     }
     else if (args[0].equals("client")) {
       Client client = new Client();
       try {
         host = InetAddress.getLocalHost();
-        client.connectToServer(host, PORT_NUMBER);
+        client.connectToServer(host, portNumber);
       } catch (UnknownHostException e) {
-        System.out.print("Could not get local host.");
+        System.out.print("Could not connect to local host.");
         System.exit(1);
       }
     } else {
